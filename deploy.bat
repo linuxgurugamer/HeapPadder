0f9093ac3c0c15e1d466910fde7a6440a3643ed0
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-09-15 17:06:23 -0400

    Added AssemblyFileVersion Updated version file for 1.12

--- deploy.bat
+++ deploy.bat
@@ -13,8 +13,12 @@ set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
 
 
+set DP0=r:\dp0\kspdev
+
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
-copy /y "%1\*.pdb" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
+
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%DP0%\GameData\%GAMEDIR%"
